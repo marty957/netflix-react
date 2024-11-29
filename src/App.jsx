4f942e61cbc3a1a50c117900d4622e13bb775382
marty957 <fantuzzimartina@yahoo.it>
2024-11-29 16:23:04 +0100

    editPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import MainSection from "./components/MainSection";
 import Footer from "./components/Footer";
+//import EditPage from "./components/EditPage";
 function App() {
   return (
     <>
       <NavBar />
+      {/*<EditPage />*/}
       <MainSection />
       <Footer />
     </>
